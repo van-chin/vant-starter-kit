ba5ea3c6ffc4385f54f36d002114d1b34edd854f
Van Chin <van.chin@qq.com>
2026-07-28 11:39:25 +0800

    feat: 增加 page 组件

---
+++ src/components/page.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page flex size-full flex-col overflow-hidden">
+    <div class="page-header" v-if="$slots.header">
+      <slot name="header"></slot>
+    </div>
+    <div class="page-content min-h-0 flex-1 overflow-y-auto">
+      <slot></slot>
+    </div>
+    <div class="page-footer" v-if="$slots.footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({ name: 'Page' });
+</script>
+
+<style scoped></style>
--- src/pages/good.vue
+++ src/pages/good.vue
@@ -1,41 +1,45 @@
 <template>
-  <!-- <div class="size-full overflow-hidden bd-red">
-11
-</div> -->
+  <page>
+    <template #header>
+      <van-nav-bar
+        left-text="返回"
+        right-text="分享"
+        left-arrow
+        @click-left="onClickLeft"
+        @click-right="onClickRight"
+      >
+        <template #title>
+          <van-tabs>
+            <van-tab title="详情11"></van-tab>
+            <van-tab title="参数"></van-tab>
+            <van-tab title="评论"></van-tab>
+          </van-tabs>
+        </template>
+      </van-nav-bar>
+    </template>
 
-  <van-sticky>
-    <van-nav-bar
-      left-text="返回"
-      right-text="分享"
-      left-arrow
-      @click-left="onClickLeft"
-      @click-right="onClickRight"
-    >
-      <template #title>
-        <van-tabs v-model:active="active">
-          <van-tab title="详情"></van-tab>
-          <van-tab title="参数"></van-tab>
-          <van-tab title="评论"></van-tab>
-        </van-tabs>
-      </template>
-    </van-nav-bar>
-  </van-sticky>
-  <van-button type="primary">主要按钮</van-button>
-  <van-button type="success">成功按钮</van-button>
+    <div>
+      <div class="bd-red">sss</div>
+      <div>
+        <van-button type="primary">主要按钮</van-button>
+        <van-button type="success">成功按钮</van-button>
+      </div>
+      <div class="px-2">
+        <div class="bd-red h-1000">ss</div>
+      </div>
 
-  <div class="bd-red h-1000">good 234</div>
+      <div class="bd-red">sss</div>
+    </div>
 
-  <Department></Department>
-  <div class="px-2">
-    <div class="bd-red">bottom</div>
-  </div>
-
-  <van-action-bar placeholder>
-    <van-action-bar-icon icon="chat-o" text="客服" @click="onClickIcon" />
-    <van-action-bar-icon icon="cart-o" text="购物车" @click="onClickIcon" />
-    <van-action-bar-icon icon="shop-o" text="店铺" @click="onClickIcon" />
-    <van-action-bar-button type="danger" text="立即购买" @click="onClickButton" />
-  </van-action-bar>
+    <template #footer>
+      <van-action-bar placeholder>
+        <van-action-bar-icon icon="chat-o" text="客服" @click="onClickIcon" />
+        <van-action-bar-icon icon="cart-o" text="购物车" @click="onClickIcon" />
+        <van-action-bar-icon icon="shop-o" text="店铺" @click="onClickIcon" />
+        <van-action-bar-button type="danger" text="立即购买" @click="onClickButton" />
+      </van-action-bar>
+    </template>
+  </page>
 </template>
 
 <script setup lang="ts">
