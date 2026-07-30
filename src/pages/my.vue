8cee523bc13d97f05bf36f28fb127782c53c684d
Van Chin <van.chin@qq.com>
2026-07-30 11:33:10 +0800

    feat: my page

--- src/pages/my.vue
+++ src/pages/my.vue
@@ -8,7 +8,7 @@
     </van-swipe>
     <div class="py-2">
       <van-cell-group inset>
-        <van-cell title="单元格" value="内容" />
+        <van-cell title="单元格" value="我是新修改的内容" />
         <van-cell title="商品详细示例页面" is-link to="/good" />
       </van-cell-group>
     </div>
